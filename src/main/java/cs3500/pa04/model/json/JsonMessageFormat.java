7368ba0684b99d80732d17e793d9b011d5bed44f
Nam Doan <cheezetr@gmail.com>
2023-06-06 18:48:24 -0400

    Created an interface to represent the six json formats for each method needed for the ProxyController

---
+++ src/main/java/cs3500/pa04/model/json/JsonMessageFormat.java
@@ -0,0 +1,22 @@
+package cs3500.pa04.model.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+/**
+ * Represents a Json Message Format for the six methods for BattleSalvo
+ */
+public interface JsonMessageFormat {
+  /**
+   * Returns the string representation of one of the method names
+   *
+   * @return string representation of a method name
+   */
+  String methodName();
+
+  /**
+   * Returns a JsonNode of arguments of this method
+   *
+   * @return JsonNode of arguments
+   */
+  JsonNode arguments();
+}
