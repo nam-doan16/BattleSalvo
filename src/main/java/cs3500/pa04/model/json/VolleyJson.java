6dffdfe50bc42295f03fd253e02b4a0bbed23c74
tGiech22 <tonygiech@gmail.com>
2023-06-06 19:18:18 -0400

    created JsonUtils class and the necessary JSON data definitions

---
+++ src/main/java/cs3500/pa04/model/json/VolleyJson.java
@@ -0,0 +1,20 @@
+package cs3500.pa04.model.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * JSON format of this record:
+ *
+ * <p>
+ *   <code>
+ *     {
+ *       "volley": CoordJson[]
+ *     }
+ *   </code>
+ * </p>
+ *
+ * @param volley the volley of shots represented by an array of coords
+ */
+public record VolleyJson(
+    @JsonProperty("volley") CoordJson[] volley) {
+}
---
+++ src/main/java/cs3500/pa04/model/types/FleetJson.java
@@ -0,0 +1,21 @@
+package cs3500.pa04.model.types;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import cs3500.pa04.model.json.ShipJson;
+
+/**
+ * JSON format of this record:
+ *
+ * <p>
+ *   <code>
+ *     {
+ *       "fleet": ShipJson[]
+ *     }
+ *   </code>
+ * </p>
+ *
+ * @param fleet of ships of the player represented by an array of ships
+ */
+public record FleetJson(
+    @JsonProperty("fleet") ShipJson[] fleet) {
+}
