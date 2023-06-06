fdaac16b1eb0cb204194b4e28efb42df4137ec62
Nam Doan <cheezetr@gmail.com>
2023-06-06 18:54:55 -0400

    Created the six JSON message formats (endgame, join, recorddamage, setup, successfulhits, and takeshots)

---
+++ src/main/java/cs3500/pa04/model/json/EndGameJson.java
@@ -0,0 +1,16 @@
+package cs3500.pa04.model.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
+/**
+ * Represents a Json representation of the endGame method
+ *
+ * @param methodName method name
+ * @param arguments arguments of the method
+ */
+public record EndGameJson(
+    @JsonProperty("method-name") String methodName,
+    @JsonProperty("arguments") JsonNode arguments
+) implements JsonMessageFormat {
+}
--- src/main/java/cs3500/pa04/model/json/JsonMessageFormat.java
+++ src/main/java/cs3500/pa04/model/json/JsonMessageFormat.java
@@ -1,5 +1,6 @@
 package cs3500.pa04.model.json;
 
+
 import com.fasterxml.jackson.databind.JsonNode;
 
 /**
